b4850a67d90d4677a78024a096cb30c961bb41cc
shravee gupta <61349418+shraveegupta@users.noreply.github.com>
2022-06-20 02:17:54 +0530

    Update AreaOfCircle.java

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -8,8 +8,9 @@ The value of pi is 3.14 */
 import java.util.Scanner; 
 
 class Tester {
-    public static void main(String[] args) {
-        Scanner r = new Scanner(System.in); 
+    public static void main(String[] args) 
+    {
+        Scanner r = new Scanner(System.in);// to take input 
         System.out.println("Enter Radius");
         double area;
         double pi= 3.14;
